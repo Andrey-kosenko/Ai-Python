805ef41db2ffbb718fb28ed7058375c494621bcd
sendus2space <chitergniloy@gmail.com>
2024-10-05 15:38:24 +0300

    fixed

--- promt.py
+++ promt.py
@@ -1,32 +1,25 @@
 from g4f.client import Client
-import uuid
 
-
-#vibor = input("Выбери 0 = математика, 1 = укр мова, 2 = Інформатика: ")
-#varianti = лааinput("Сколько нужно вариантов: ")
-#skolko = input("Сколько будет вопросов: ")
-#vopros = input("Напиши вопрос: ")
-
-uchitel = ""
-if vibor == '0':
-    uchitel = "математики"
-elif vibor == '1':
-    uchitel = 'укр мова'
-elif vibor == '2':
-    uchitel = 'Інформатики'
-else:
-    print("Неверный ввод")
-    exit()
+predmet = input("Предмет? ")
+klas = input("Класс? ")
+varianti = input("Сколько вариантов? ")
+skolko = input("Сколько задач в каждом варианте? ")
+vopros = input("Тип задач? ")
 
 client = Client()
+
+prompt = (f"Предложи {varianti} разных примеров уравнений для {klas} класса по предмету {predmet}. "
+          f"В каждом примере должно быть по {skolko} уравнений. "
+          f"Тип уравнений похож на {vopros}, но числа в каждом примере должны быть разные. "
+          f"Убедись, что формат уравнений одинаковый: с пробелами перед и после знаков '=', '+' и '-'. "
+          f"Пример формата: '2х + 3 = 9'. Ответ должен быть структурирован по следующим правилам: "
+          f"Вариант 1:\n1. Уравнение 1\n2. Уравнение 2\n... "
+          f"Вариант 2:\n1. Уравнение 1\n2. Уравнение 2\n...")
+
 response = client.chat.completions.create(
-    model="gpt-4",
-    messages=[{
-        "role": "user",
-        "content": ""
-    }],
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}],
 )
 
 content = response.choices[0].message.content
-
-print(content)  
+print(content)
